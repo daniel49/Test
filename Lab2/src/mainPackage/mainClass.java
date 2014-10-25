733e087178657c72e26c2d7d62f18a244631fe3e
daniel49 <ilie.daniel49@yahoo.com>
2014-10-25 18:28:11 +0300

    Created package with class file, with base comments

---
+++ Lab2/src/mainPackage/mainClass.java
@@ -0,0 +1,23 @@
+package mainPackage;
+/**
+ * 
+ * @author Ilie Daniel
+ * @since 25/10/2014
+ * @version 1.0
+ * @category Laboratory homework
+ *
+ */
+
+public class mainClass {
+
+	/**
+	 * @param args - Function has no arguments
+	 * @return Function has no return value
+	 * @throws Function does not throw an exception
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main function");
+	}
+
+}
